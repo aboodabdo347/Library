8ffa826649da253bc417a9391c9c2b223de0822a
Ali Albanna <ali@albanna.com>
2024-03-22 20:29:47 +0300

    intro done

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,5 +1,6 @@
 import defaultBook from "../images/defaultBook.jpg"
 import Rating from '@mui/material/Rating';
+import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
 
 const Intro = () => {
 
@@ -9,26 +10,29 @@ const Intro = () => {
                 <div className="row">
                     <div className="col">
                     <h1 className="display-1 text-start">
-                        SHARE TO OTHERS, LEARN FROM OTHERS
+                        SHARE TO OTHERS, DISCOVER WITH OTHERS
                     </h1>
+                    <div className="mt-5 d-flex">
+                        <button className="btn btn-outline-secondary mr-3">Start Discovering <ArrowOutwardIcon /></button>
+                        <button className="btn btn-outline-secondary mx-3">Become an Author</button>
+                    </div>
                     </div>
                     <div className="col d-flex flex-column">
-                        <img className="intro-book border" src={defaultBook} alt="book image" />
-                        <div className="d-flex">
+                        <div className="intro-img">
+                            <img className="intro-book border" src={defaultBook} alt="book image" />
+                        </div>
+                        <div className="d-flex justify-content-between">
                             <div className="text-start mt-3 intro-rating-bg">
                                 <div className="d-flex justify-content-between">
                                     <div>
                                         <span className="intro-rate-num mx-2">4.0</span>
                                         <Rating className="stars-intro" name="read-only" value="4" readOnly size="small" />
                                     </div>
+                                    <div>
+                                        <button className="btn btn-outline-success btn-sm ">View</button>
+                                    </div>
                                 </div>
-                                <p className="intro-rate-solgan">The life-changing million-copy #1 bestseller</p>
-                                {/* <div>
-                                    <button className="btn btn-outline-success btn-sm ">View</button>
-                                </div> */}
-                            </div>
-                            <div>
-                                
+                                <p className="intro-rate-solgan">He calls them atomic habits. In this ground-breaking book, Clears reveals exactly how these minuscule changes can grow into such life-altering outcomes.</p>
                             </div>
                         </div>
                     </div>
