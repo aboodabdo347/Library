559fce492cdb6f4af4b99aa0baa3da711b789490
Ali Albanna <ali@albanna.com>
2024-03-23 23:04:36 +0300

    create profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css"
 import { Route, Routes, useNavigate } from "react-router-dom"
 import Home from "./pages/Home"
+import Profile from "./pages/Profile"
 
 const App = () => {
 	return(
         <Routes>
             <Route path="/" element={ <Home /> } />
+            <Route path="/profile" element={<Profile />} />
         </Routes>
 	)
 
