ae37ae7507e556fa91911828865fae9c1da1a6a3
Ali Albanna <ali@albanna.com>
2024-03-23 11:54:05 +0300

    discover component

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -1,7 +1,16 @@
 const Discover = () => {
     return (
         <div>
-            Discover
+            <div className="container text-center p-5 justify-content-center mt-5">
+                <div className="grid row column-gap-5">
+                    <div className="col text-start display-5">
+                        Get Insights from people of all expertise
+                    </div>
+                    <div className="col text-start">
+                        The key to your next adventure awaits. Unlock a universe of stories, knowledge, and inspiration with our vast collection. Start exploring today!
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ const Home = () => {
         <div>
             <Intro />
             <About />
+            <Discover />
         </div>
     )
 }
