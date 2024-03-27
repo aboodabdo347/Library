bc1011a8d4c33685a1ecde22db5e50d8fe5c6231
Ali Albanna <ali.al-banna@outlook.com>
2024-03-27 15:08:54 +0300

    book title showing inside each collection

--- src/pages/DiscoverCollections.jsx
+++ src/pages/DiscoverCollections.jsx
@@ -6,12 +6,34 @@ const DiscoverCollections = () => {
 
   const getAllCollections = async () => {
     let getCollRes = await Client.get("/collections")
-    console.log(getCollRes.data)
+    setCollections(getCollRes.data)
+    // console.log(getCollRes.data)
   }
 
   useEffect(() => {
     getAllCollections()
-  })
+  }, [])
+
+  return (
+    <div className="container">
+      <div className="row mt-5">
+        {collections
+          ? collections.map((collection) => {
+              return (
+                <div key={collection._id}>
+                  <h4>{collection.title}</h4>
+                  <div className="overflow-x-scroll d-flex my-5">
+                    <div className="d-flex">
+                      {collection.books.map((book) => book.title)}
+                    </div>
+                  </div>
+                </div>
+              )
+            })
+          : null}
+      </div>
+    </div>
+  )
 }
 
 export default DiscoverCollections
