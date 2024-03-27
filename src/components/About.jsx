de69c1820a525ab56ee3df2c7ea8c54d4c0aca75
Ali Albanna <ali.al-banna@outlook.com>
2024-03-27 16:46:43 +0300

    discover collections working

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,38 +1,58 @@
-import PeopleIcon from '@mui/icons-material/People';
-import CreateIcon from '@mui/icons-material/Create';
-import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
-import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
-
+import PeopleIcon from "@mui/icons-material/People"
+import CreateIcon from "@mui/icons-material/Create"
+import ConnectWithoutContactIcon from "@mui/icons-material/ConnectWithoutContact"
+import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward"
+import { useNavigate } from "react-router-dom"
 
 const About = () => {
-    return (
-        <div className="about-bg mt-5 p-5">
-            <div className="container text-center p-5 justify-content-center">
-                <div className="row">
-                    <div className="col d-flex flex-column justify-content-center">
-                        <h1 className="display-6 text-start dt">
-                            A Platform for Everyone
-                        </h1>
-                        <p className="text-start">Readers and authors alike can connect with others who share similar interests, passions, and expertise.</p>
-                        <div className="mt-5 d-flex">
-                        <button className="btn btn-outline-secondary">Discover Collections <ArrowOutwardIcon /></button>
-                    </div>
-                    </div>
-                    <div className="col d-flex flex-column align-items-center">
-                        <div className="about-item">
-                            <p className="mt-3 px-3 d-flex"><PeopleIcon /><span className='mx-4'>Readers choose their kind of content.</span></p>
-                        </div>
-                        <div className="about-item mt-3">
-                            <p className="mt-3 px-3 d-flex"><CreateIcon /><span className='mx-3'>Authors secure their intellectual property.</span></p>
-                        </div>
-                        <div className="about-item mt-3">
-                            <p className="mt-3 px-3 d-flex"><ConnectWithoutContactIcon /><span className='mx-2'>Connecting communities around the globe.</span></p>
-                        </div>
-                    </div>
-                </div>
+  const goToAllCollections = () => {}
+
+  return (
+    <div className="about-bg mt-5 p-5">
+      <div className="container text-center p-5 justify-content-center">
+        <div className="row">
+          <div className="col d-flex flex-column justify-content-center">
+            <h1 className="display-6 text-start dt">A Platform for Everyone</h1>
+            <p className="text-start">
+              Readers and authors alike can connect with others who share
+              similar interests, passions, and expertise.
+            </p>
+            <div className="mt-5 d-flex">
+              <button className="btn btn-outline-secondary">
+                Discover Collections <ArrowOutwardIcon />
+              </button>
             </div>
+          </div>
+          <div className="col d-flex flex-column align-items-center">
+            <div className="about-item">
+              <p className="mt-3 px-3 d-flex">
+                <PeopleIcon />
+                <span className="mx-4">
+                  Readers choose their kind of content.
+                </span>
+              </p>
+            </div>
+            <div className="about-item mt-3">
+              <p className="mt-3 px-3 d-flex">
+                <CreateIcon />
+                <span className="mx-3">
+                  Authors secure their intellectual property.
+                </span>
+              </p>
+            </div>
+            <div className="about-item mt-3">
+              <p className="mt-3 px-3 d-flex">
+                <ConnectWithoutContactIcon />
+                <span className="mx-2">
+                  Connecting communities around the globe.
+                </span>
+              </p>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default About
--- src/pages/DiscoverCollections.jsx
+++ src/pages/DiscoverCollections.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import Client from "../services/api"
+import { ConstructionOutlined } from "@mui/icons-material"
 
 const DiscoverCollections = () => {
   const [collections, setCollections] = useState(null)
@@ -19,16 +20,42 @@ const DiscoverCollections = () => {
       <div className="row mt-5">
         {collections
           ? collections.map((collection) => {
-              return (
-                <div key={collection._id}>
-                  <h4>{collection.title}</h4>
-                  <div className="overflow-x-scroll d-flex my-5">
-                    <div className="d-flex">
-                      {collection.books.map((book) => book.title)}
+              if (collection.books.length > 0) {
+                return (
+                  <div key={collection._id}>
+                    <div className="d-flex justify-content-between">
+                      <h4>{collection.title}</h4>
+                      <button className="btn btn-outline-secondary">
+                        View Collection
+                      </button>
                     </div>
+                    <div className="overflow-x-scroll d-flex my-5 collection-all-background">
+                      <div className="d-flex">
+                        {collection.books.map((book) => {
+                          let keyId =
+                            book._id + Math.floor(Math.random() * 99999999)
+                          return (
+                            <div
+                              key={keyId}
+                              className="card book-search-default d-flex align-items-center m-2"
+                            >
+                              <img
+                                src={book.image}
+                                className="card-img-top discover-book-img"
+                                alt={book.title}
+                              />
+                              <div className="card-body overflow-y-auto">
+                                <p className="card-text ">{book.title}</p>
+                              </div>
+                            </div>
+                          )
+                        })}
+                      </div>
+                    </div>
+                    <hr />
                   </div>
-                </div>
-              )
+                )
+              }
             })
           : null}
       </div>
