7a1cee282d3307161b7428189c5ddba2c5ac22fd
Ali Albanna <ali.al-banna@outlook.com>
2024-03-27 14:56:20 +0300

    get collections form db

--- src/pages/DiscoverCollections.jsx
+++ src/pages/DiscoverCollections.jsx
@@ -1,7 +1,17 @@
 import { useState, useEffect } from "react"
+import Client from "../services/api"
 
 const DiscoverCollections = () => {
   const [collections, setCollections] = useState(null)
+
+  const getAllCollections = async () => {
+    let getCollRes = await Client.get("/collections")
+    console.log(getCollRes.data)
+  }
+
+  useEffect(() => {
+    getAllCollections()
+  })
 }
 
 export default DiscoverCollections
